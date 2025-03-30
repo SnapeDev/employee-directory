88d5668a53dc1a26ca2507f40751513bed063e5d
Jack <jacksnape89@gmail.com>
2025-03-30 11:08:16 +0100

    latest

--- obj/Debug/net9.0/EmployeeDirectory.AssemblyInfo.cs
+++ obj/Debug/net9.0/EmployeeDirectory.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EmployeeDirectory")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+acc61708f6e88d4457bc225669cf1c3ae738f567")]
 [assembly: System.Reflection.AssemblyProductAttribute("EmployeeDirectory")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EmployeeDirectory")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
